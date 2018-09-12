305a27748b0207ae47f7f6bcbaaec56c5d4c87ea
Ryan Healy <ryan.healy11@gmail.com>
2018-09-12 16:11:45 -0700

    more fun stuuf

--- Project7/Source.cpp
+++ Project7/Source.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-#include <limits>
+
+bool containsValue(int value, int * arr, int arrSize)
+{
+	for (int i = 0; i < arrSize; i++)
+	{
+		if (arr[i] == value)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
 
 int main()
 {
+	int nums[] = { 0, 5, 7, 1, 2, 3 };
+
+	bool result = containsValue(7, nums, 6);
+
+	std::cout << "The value was " << (result ? "found" : "not found") << "!" << std::endl;
 
 	return 0;
 }
